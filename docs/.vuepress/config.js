f7ba4ecee191cbc6180133fb8727cac88507fea9
Feng Wang <fengwang324@gmail.com>
2023-11-13 20:39:14 -0800

    update title

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,6 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
   base:'/',
   lang: 'zh-CN',
-  title: '你好 VuePress',
+  title: 'Deyuherbs',
   description: '站点测试',
 })
